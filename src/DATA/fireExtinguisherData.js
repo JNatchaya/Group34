8d9fe56d2a84bd387d26224244f349062e52e18b
Siksen-Sarutisuta <siksen.sar@spumail.net>
2024-11-20 23:29:55 +0700

    Data fixed

--- src/DATA/fireExtinguisherData.js
+++ src/DATA/fireExtinguisherData.js
@@ -5,20 +5,21 @@ export const fire_extinguisher_data = [
         "DPName": "C1_Department_01",
         "fire": [
             {
-                "fireID": "001",
                 "type": "CO2",
                 "location": "Floor 1 - Aisle 5",
                 "lastMaintenance": "2024-10-01",
                 "nextMaintenance": "2025-01-01",
-                "serialNumber": "SN123456"
+                "serialNumber": "SN123456",
+                "status": "Active"
             },
             {
-                "fireID": "002",
                 "type": "Dry Chemical",
                 "location": "Floor 2 - Room 205",
                 "lastMaintenance": "2024-09-15",
                 "nextMaintenance": "2024-12-15",
-                "serialNumber": "SN789012"
+                "serialNumber": "SN789012",
+                "status": "Inactive"
+
             }
         ]
     },
@@ -26,20 +27,21 @@ export const fire_extinguisher_data = [
         "DPName": "C1_Department_02",
         "fire": [
             {
-                "fireID": "003",
                 "type": "Foam",
                 "location": "Floor 3 - Near Elevator",
                 "lastMaintenance": "2024-10-10",
                 "nextMaintenance": "2025-01-10",
-                "serialNumber": "SN345678"
+                "serialNumber": "SN345678",
+                "status": "Active"
+
             },
             {
-                "fireID": "004",
                 "type": "Water",
                 "location": "Floor 1 - Lobby",
                 "lastMaintenance": "2024-09-20",
                 "nextMaintenance": "2024-12-20",
-                "serialNumber": "SN901234"
+                "serialNumber": "SN901234",
+                "status": "Inactive"
             }
         ]
     },
@@ -47,12 +49,12 @@ export const fire_extinguisher_data = [
         "DPName": "C2_Department_01",
         "fire": [
             {
-                "fireID": "005",
                 "type": "CO2",
                 "location": "Floor 1 - Aisle 5",
                 "lastMaintenance": "2024-10-01",
                 "nextMaintenance": "2025-01-01",
-                "serialNumber": "SN567890"
+                "serialNumber": "SN567890",
+                "status": "Active"
             }
         ]
     },
@@ -60,12 +62,12 @@ export const fire_extinguisher_data = [
         "DPName": "C2_Department_02",
         "fire": [
             {
-                "fireID": "006",
                 "type": "Dry Chemical",
                 "location": "Floor 2 - Room 205",
                 "lastMaintenance": "2024-09-15",
                 "nextMaintenance": "2024-12-15",
-                "serialNumber": "SN012345"
+                "serialNumber": "SN012345",
+                "status": "Inactive"
             }
         ]
     }
