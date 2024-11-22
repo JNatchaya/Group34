db79ffde7cf1fc7ef9b791ab84952cc28eff3783
JNatchaya <sky22jn@gmail.com>
2024-11-22 23:23:20 +0700

    M-G-Update style

--- src/Permisstion/Mechanic/Nearly-place/nearly_place.jsx
+++ src/Permisstion/Mechanic/Nearly-place/nearly_place.jsx
@@ -1,24 +1,33 @@
 import React from "react";
-import '../layout/M-layout.css';
+import "../layout/M-layout.css";
+import "../Assignment/assignmentnearly.css";
 
-function NearlyPlace({ onReturn }) {
+function NearlyPlace({ onReturn, onTaskClick }) {
   return (
-    <div className="nearly-place">
+    <div className="nearly-place-container">
       <h2>Assignment Nearly</h2>
       <div className="place-section">
         <h3>Place</h3>
         {[...Array(5)].map((_, index) => (
-          <div key={index} className="task-card2">
-            <span className="task-title">fire extinguisher Serial number</span>
+          <div
+            key={index}
+            className="task-card2"
+            onClick={onTaskClick} // ไปหน้า AssignmentInfo
+          >
+            <span className="task-title">Fire extinguisher Serial number {index + 1}</span>
             <label className="finish">
-              <span className="label">finish</span>
+              <span className="label">Finish</span>
               <input type="checkbox" />
             </label>
           </div>
         ))}
       </div>
-      <button className="return-button" onClick={onReturn}>
-        return
+
+      <button
+        className="return-button"
+        onClick={onReturn} //กลับไป AssignmentNearly
+      >
+        Return
       </button>
     </div>
   );
