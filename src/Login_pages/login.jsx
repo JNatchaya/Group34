ac40e4a9dd9424dfa157b420fbfdc8b0ddb71594
Thanoka <Normallifethan@gmail.com>
2024-11-27 19:12:22 +0700

    .

--- src/Login_pages/login.jsx
+++ src/Login_pages/login.jsx
@@ -18,7 +18,7 @@ function Login({ token, setToken }) {
       <aside className="info-space">
         <div className="Logo"></div>
         <div className="info-text">
-          <h2>Lorem ipsum dolor sit.</h2>
+          <span className="h">More About Us</span>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Necessitatibus praesentium labore ex, quae tempore architecto sunt!
