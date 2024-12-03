ef98db11faef170ef0cf80fab5bd034cbccdd6db
JNatchaya <sky22jn@gmail.com>
2024-12-03 18:57:01 +0700

    34fe1

--- src/init.jsx
+++ src/init.jsx
@@ -12,7 +12,7 @@ import "./init.css";
 function Init() {
   const storedToken = localStorage.getItem('token');
   const [token, setToken] = useState(storedToken || '');  
-  localStorage.setItem('token', 'SuperUser');
+  localStorage.setItem('token', '');
  
   // clearToken()
  
